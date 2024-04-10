88a3223cb289a1255d1a45963eac4a91f53bdbd8
Lilayasuff <lyasuff@udesa.edu.ar>
2024-04-10 10:36:56 -0300

    empece con la base de datos,falta terminar de poner algunos datos pero la estuctura esta lista

---
+++ pi-g9/db/moduloDatos.js
@@ -0,0 +1,142 @@
+ const base = {
+    usuarios: {
+        id: 1,
+        email: 'pepitojuarez@gmail.com',
+        password: 'pep2021',
+        fecha: '20/04/2003',
+        DNI: '45887934',
+        foto: ''
+    },
+    productos: [
+        { id: 1,
+          imagen: "",
+          producto: "Adidas forum mid",
+          descripcion: 'Son una opción icónica que combina estilo y rendimiento. Las Adidas Forum Mid son un ícono del estilo urbano que se remonta a los años 80, cuando fueron lanzadas originalmente como zapatillas de baloncesto de alto rendimiento. Con su diseño de corte medio, las Forum Mid ofrecen un excelente soporte al tobillo, lo que las hace ideales para el uso diario y para actividades deportivas.',
+          comentarios:[
+            {
+                id: 1,
+                nombreusuario: "",
+                imagen: '',
+                descripcion: "",
+            }
+          ]
+        },
+        { id: 2,
+          imagen: "",
+          producto: "Adidas campus",
+          descripcion: 'Las Adidas Campus son unas zapatillas clásicas que se han convertido en un icono del estilo urbano desde su lanzamiento en la década de 1980. Con un diseño elegante y atemporal, las Campus destacan por su parte superior de gamuza suave y su silueta baja, que ofrece un ajuste cómodo y versáti',
+          comentarios:[
+            {
+                id: 2,
+                nombreusuario: "",
+                imagen: '',
+                descripcion: "",
+            }
+          ]
+         },
+        { id: 3,
+          imagen: "",
+          producto: "Nike jordan air 1 mid",
+          descripcion: 'Las Nike Jordan Air 1 Mid son una versión del icónico modelo Air Jordan 1 de Nike, diseñadas para ofrecer un equilibrio entre estilo clásico y rendimiento moderno. Con un diseño de corte medio, estas zapatillas brindan un soporte adicional al tobillo, lo que las hace ideales para el uso diario y la práctica del baloncesto. ',
+          comentarios:[
+            {
+                id: 3,
+                nombreusuario: "",
+                imagen: '',
+                descripcion: "",
+            }
+          ]
+         },
+        { id: 4,
+          imagen:'' ,
+          producto: "Blazer nike mid 77",
+          descripcion: 'Las Nike Blazer Mid 77 son una versión retro del clásico modelo Blazer de Nike, que originalmente se lanzó en 1977 como una zapatilla de baloncesto de alto rendimiento. Con un diseño de corte medio, estas zapatillas ofrecen un estilo vintage y un ajuste cómodo que las hace ideales tanto para el uso diario como para actividades deportivas casuales.',
+          comentarios:[
+            {
+                id: 4,
+                nombreusuario: "",
+                imagen: '',
+                descripcion: "",
+            }
+          ]
+        },
+        { id: 5,
+          imagen: '',
+          producto: "Adidas gazelle",
+          descripcion: 'Las Adidas Gazelle son un clásico del calzado urbano que se lanzó originalmente en 1968 como zapatillas de entrenamiento para fútbol sala. Con un diseño de perfil bajo y una estética retro, las Gazelle son conocidas por su simplicidad y versatilidad. ',
+          comentarios:[
+            {
+                id: 5,
+                nombreusuario: "",
+                imagen: '',
+                descripcion: "",
+            }
+          ]
+         },
+        { id: 6,
+          imagen: '',
+          producto: "Air Jordan 1 Low",
+          descripcion: 'Las Air Jordan 1 Low ofrecen un estilo retro y una versatilidad que las convierten en una opción imprescindible para cualquier aficionado al calzado urbano y a la moda deportiva.',
+          comentarios:[
+            {
+                id: 6,
+                nombreusuario: "",
+                imagen: '',
+                descripcion: "",
+            }
+          ]
+        },
+        { id: 7,
+          imagen: "",
+          producto: "Adidas forum low",
+          descripcion: 'Las Adidas Forum Low son unas zapatillas de estilo retro con un diseño de corte bajo. Fabricadas con cuero de alta calidad y detalles icónicos de Adidas, ofrecen un aspecto clásico y versátil',
+          comentarios:[
+            {
+                id: 7,
+                nombreusuario: "",
+                imagen: '',
+                descripcion: "",
+            }
+          ]
+        },
+        { id: 8,
+          imagen: "",
+          producto: "New Balance 550",
+          descripcion: 'Las New Balance 550 son zapatillas de estilo retro que combinan una estética clásica con detalles modernos. Confeccionadas con materiales de alta calidad, como cuero y gamuza, las 550 ofrecen durabilidad y estilo.',
+          comentarios:[
+            {
+                id: 8,
+                nombreusuario: "",
+                imagen: '',
+                descripcion: "",
+            }
+          ]
+        },
+        { id: 9,
+          imagen: "",
+          producto: "Nike dunk low",
+          descripcion: 'Las Nike Dunk Low son zapatillas de estilo clásico y versátil. Con un diseño de corte bajo, ofrecen un aspecto casual y cómodo para el día a día',
+          comentarios:[
+            {
+                id: 9,
+                nombreusuario: "",
+                imagen: '',
+                descripcion: "",
+            }
+          ]
+        },
+        { id: 10,
+          imagen: "",
+          producto: "Air Jordan Legacy 312 Low",
+          descripcion: 'Estas zapatillas combinan elementos de varios modelos clásicos de Nike y Jordan para crear un diseño único y llamativo.',
+          comentarios:[
+            {
+                id: 10,
+                nombreusuario: "",
+                imagen: '',
+                descripcion: "",
+            }
+          ]
+      },
+    ]
+ }
